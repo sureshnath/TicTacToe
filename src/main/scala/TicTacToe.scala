44c7b8f05c7494a8e5283c7f140d6d334db20f08
Suresh Jaganathan <Suresh Jaganathan@globalrelay.net>
2021-06-17 14:48:12 +0100

    with board initialised

--- src/main/scala/TicTacToe.scala
+++ src/main/scala/TicTacToe.scala
@@ -1,6 +1,7 @@
 package com.example.ttt
 
 class TicTacToe() {
+  var board: Array[Array[Char]] = Array.ofDim[Char](3,3)
   def result(): String = {
     "no winner"
   }
--- src/test/scala/TicTacToeSuite.scala
+++ src/test/scala/TicTacToeSuite.scala
@@ -5,6 +5,7 @@ import org.scalatest.funsuite.AnyFunSuite
 class TicTacToeSuite extends AnyFunSuite{
   test("An empty board test") {
     val board = new TicTacToe()
+    board.board = Array(Array(' ',' ',' '),Array(' ',' ',' '),Array(' ',' ',' '))
     assert(board.result().equals("no winner"))
   }
 }
